c819a2b3704125ec193ffe6ad7391aa852f11cbc
euxxnia <syoomch12@gmail.com>
2024-10-23 01:35:43 +0900

    update works data

--- src/data/works.tsx
+++ src/data/works.tsx
@@ -16,7 +16,8 @@ export const works: Work[] = [
     date: '2024',
     keywords: ['UXUI Design'],
     image: '/CSEREAL.png',
-    content: 'Work 1 content...',
+    content:
+      '서울대학교 컴퓨터공학부 홈페이지 리뉴얼 프로젝트에 디자인과 기획으로 참여\n\n5명의 개발자와 2명의 디자이너로 이루어진 팀에서 협업 중이다.',
   },
   {
     id: 2,
@@ -25,7 +26,8 @@ export const works: Work[] = [
     date: '2023~',
     keywords: ['Product Design'],
     image: '/SNUTT.png',
-    content: 'Work 2 content...',
+    content:
+      'SNUTT는 와플스튜디오에서 개발한 MAU 2만 명의 서울대생 필수 시간표 어플이다.\n2023년부터 프로덕트 디자이너를 맡으며 11명의 개발자와 1명의 디자이너와 함께 서비스를 만들어나가고 있다.\n기획과 UXUI 디자인 외에도 전반적인 디자인을 맡고 있다.',
   },
   {
     id: 3,
@@ -34,7 +36,8 @@ export const works: Work[] = [
     date: '2024',
     keywords: ['Starter Kit Design & Web'],
     image: '/DesigningAProduct.png',
-    content: 'Work 2 content...',
+    content:
+      '프로덕트 개발이라는 분야에 대한 나름의 해석을 추상적인 이미지로 표현한 작품',
   },
   {
     id: 4,
@@ -43,7 +46,8 @@ export const works: Work[] = [
     date: '2024',
     keywords: ['Hackathon: UXUI Design, LLM'],
     image: '/Icebreaker.png',
-    content: 'Work 3 content...',
+    content:
+      '해커톤에서 4명의 개발자와 함께 만든 아이스브레이킹 웹\nLLM 어쩌고',
   },
   {
     id: 5,
